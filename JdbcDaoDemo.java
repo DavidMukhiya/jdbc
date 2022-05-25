aad33294c303a80cb12f4dc8739236bee9bda5b2
DavidMukhiya <sherlock.41v94@gmail.com>
2022-05-25 16:34:15 -0700

    updating table using jdbc and dao pattern

--- JdbcDaoDemo.java
+++ JdbcDaoDemo.java
@@ -2,6 +2,7 @@ package com.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,24 +11,34 @@ import java.sql.Statement;
 public class JdbcDaoDemo {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		StudentDao dao = new StudentDao();
+		dao.connect();
 		Student s1 = dao.getStudent(5);
 		System.out.println(s1.sname);
+		Student s2 = new Student();
+		s2.rollno = 2;
+		s2.sname = "Nishant";
+		
+		dao.addStudent(s2);
 		
 	}
 }
 
 class StudentDao{
+	
+	Connection con = null;
+	
+	public void connect() throws ClassNotFoundException, SQLException {
+		String url = "jdbc:mysql://localhost/student_manage";
+		String uName = "root";
+		String passWord = "";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		con = DriverManager.getConnection(url, uName, passWord);
+	}
 	public Student getStudent(int rollno) throws ClassNotFoundException, SQLException {
 		Student s = new Student();
 		s.rollno = rollno;
 		
-		String url = "jdbc:mysql://localhost/student_manage";
-		String uName = "root";
-		String passWord = "";
 		String query = "select sName from studentnew where rollNo="+rollno;
-		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con = DriverManager.getConnection(url, uName, passWord);
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery(query);
 		rs.next();
@@ -36,6 +47,14 @@ class StudentDao{
 		
 		
 		return s;
+	}
+	
+	public void addStudent(Student s) throws SQLException {
+		String query = "insert into studentnew values (?,?)";
+		PreparedStatement pst = con.prepareStatement(query);
+		pst.setInt(1, s.rollno);
+		pst.setString(2, s.sname);
+		pst.executeUpdate();
 	}
 }
 
